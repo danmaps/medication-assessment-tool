4df8b2d82c033a032e6abe664aaa47dbd3458304
Danny McVey <dmcvey@esri.com>
2019-10-07 14:52:38 -0700

    optimize

--- automation/mats.py
+++ automation/mats.py
@@ -13,32 +13,10 @@ import os
 # In[2]:
 
 
-def use_template(template, newfilename, context):
+def make_docx(template, newfilename, context):
     doc = DocxTemplate(template)
     doc.render(context)
     doc.save(newfilename)
-
-
-# basic test based on https://docxtpl.readthedocs.io/
-
-# template = r"docxtpl.docx" # contains only "{{ tag }}"
-# newfilename = "generated_doc.docx"
-# context = {'tag':'i am a computer'}
-
-# use_template(template,newfilename,context)
-
-# def search_file(filename,text):
-#     doc = Document(filename)
-#     for p in doc.paragraphs:
-#         if context['tag'] in p.text:
-#             found = True
-#     return found
-
-# assert(search_file(newfilename,context['tag']))
-# print("✔️test passed")
-
-# clean up
-# os.remove(newfilename)
 
 
 # In[3]:
@@ -57,21 +35,30 @@ def parse_excel_data(in_file):
         dict(zip(columns, row))
         for row in open_sheet.iter_rows(min_row=2, values_only=True)
     ]
-
-    #     for x in dictlist:
-    #         missing = ", ".join(set(keys).difference(x.keys()))
-    #         msg = " missing from excel spreadsheet"
-    #         assert all(k in x.keys() for k in keys), missing + msg
-    #         for k in x.keys():
-    #             if k not in keys:
-    #                 warnings.warn(k + " is not in the template")
-    #             if not x[k]:
-    #                 warnings.warn(k + " is null")
-
     return dictlist
 
 
+# In[5]:
+
+
+def combine_all_docx(filename_master, files_list):
+    from datetime import datetime
+
+    number_of_sections = len(files_list)
+    master = Document(filename_master)
+    composer = Composer(master)
+    for i in range(0, number_of_sections):
+        doc_temp = Document(files_list[i])
+        composer.append(doc_temp)
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    outputfile = os.path.join(r"..", f"mats_{now}.docx")
+    composer.save(outputfile)
+    print(outputfile)
+
+
 # In[4]:
+
+
 context = {
     "medication": "",
     "ref": "",
@@ -95,41 +82,15 @@ for med in parse_excel_data(os.path.join("..", "mats.xlsx")):
             context[col] = med[col]
         else:
             context[col] = ""
-    # context = med
-    dropbox = r".."
-    template = "mat_template.docx"
-    output = os.path.join(dropbox, "MAT" + str(med["medication"]) + ".docx")
-    # output = os.path.join("MAT"+str(med["medication"])+".docx")
+    output = os.path.join(r"..", "MAT" + str(med["medication"]) + ".docx")
 
-    use_template(template, output, context)
-    # print(output)
+    make_docx("mat_template.docx", output, context)
     outputs.append(output)
-####################
-
 
 # insert page break after every 2 mats
 for i in range(len(outputs) + (int(len(outputs) / 2))):
     if (i + 1) % 3 == 0:
         outputs.insert(i, "pagebreak.docx")
-
-
-# In[5]:
-
-
-def combine_all_docx(filename_master, files_list):
-    from datetime import datetime
-
-    number_of_sections = len(files_list)
-    master = Document(filename_master)
-    composer = Composer(master)
-    for i in range(0, number_of_sections):
-        doc_temp = Document(files_list[i])
-        composer.append(doc_temp)
-    now = datetime.now().strftime("%Y%m%d-%H%M%S")
-    outputfile = os.path.join(dropbox, f"mats_{now}.docx")
-    composer.save(outputfile)
-    print(outputfile)
-
 
 # print(outputs)
 combine_all_docx("merge.docx", outputs)
